963effdcbd24acf8c1045ac8e97a9f92bc975bd0
Ryan Wilson <RryanWilsonW@gmail.com>
2021-04-07 10:20:32 -0400

    Basic react server set up with a minimal header component, bootstrap required and installed, and API directory set up.

---
+++ exhibiting_employees/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Employee Directory</h1>
+            <p>Instructions for website.</p>
+        </header>
+    );
+};
+
+export default Header;
