0063f6c5e578c0bda89491511c16029b9294078f
Ryan Wilson <RryanWilsonW@gmail.com>
2021-04-11 05:10:02 -0400

    Using API with axios, we are pulling our employee records into the App.js file, and console.loggin them.

--- exhibiting_employees/src/App.js
+++ exhibiting_employees/src/App.js
@@ -4,17 +4,48 @@ import Wrapper from './components/Wrapper';
 import EmployeeCard from './components/PageBody';
 import Search from './components/Search';
 import './styles/app.css';
+import API from './utils/API';
+import React from 'react';
 
-function App() {
-  return (
-    <div className='app'>
-      <Wrapper>
-        <Header />
-        <Search />
-        <EmployeeCard />
-      </Wrapper>
-    </div>
-  );
+class App extends React.Component {
+  state = { result: {}, search:'' }
+
+  componentDidMount = () => {
+    API.getEmployees()
+      .then((records) => {
+        let data = records.data.results;
+        let employeeData = [records.data.results];
+        for (let i = 0; i < data.length; i++) {
+          let card = {
+            name: data[i].name.first + " " + data[i].name.last,
+            email: data[i].email,
+            image: data[i].picture.medium,
+            phone: data[i].cell,
+            dob: data[i].dob.date,
+          };
+          employeeData.push(card);
+        }
+        console.log(employeeData);
+        this.setState({ employeeRecords: employeeData });
+      });
+  };
+
+  getEmployees = query => {}
+  render() {
+    return (
+      <div className='app'>
+        <Wrapper>
+          <Header />
+          <Search />
+          {this.state.employeeData.map(employee => (
+            <EmployeeCard 
+            name={employee.name}
+            email={employee.email}/>
+          ))}
+        </Wrapper>
+      </div>
+    );  
+  }
 }
 
 export default App;
